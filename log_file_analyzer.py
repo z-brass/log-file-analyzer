6a67bd494cc818ed5d02f45b2732d5cfb068cb57
nauticalzakku <kidraul319419@hotmail.com>
2026-09-05 15:39:45 -0400

    started converting to OOP

--- log_file_analyzer.py
+++ log_file_analyzer.py
@@ -2,21 +2,10 @@ import re # regex
 from datetime import datetime, timedelta
 from pathlib import Path
 
-## global variables ##
-info_count = 0
-warning_count = 0
-error_count = 0
-## max time window before brute force is detected ##
-FIVE_MINS = timedelta(minutes = 5)
-FAILED_ATTEMPT_THRESHOLD = 5
+## functions ##
+## parse_log_line(), log_entries(), build_failed_login_data(), detect_bruce_force(), alerts(), generate_report(), main()
 
-
-log_entries = []
-
-## creating a dictionary for failed logins per ip
-failed_logins_by_ip = {}
-
-## layout ##
+## dictionary idea ##
 '''
 failed_logins_by_ip = {
     "192.169.1.25": {
@@ -30,98 +19,103 @@ failed_logins_by_ip = {
     }
 }
 '''
-#########################
-## adding portability for reading the log file ##
-LOG_FILE = Path(__file__).parent / "sample.log"
 
-with open(LOG_FILE, "r") as file:
-    for line in file:
+################################################################
 
-        if line.startswith("#"):
-            continue
+## GLOBALS ##
+info_count = 0
+warning_count = 0
+error_count = 0
+## max time window before brute force is detected ##
+FIVE_MINS = timedelta(minutes = 5)
+FAILED_ATTEMPT_THRESHOLD = 5
 
-        # debugging the blank line error when trying to pull parts 
-        if not line.strip():
-            continue
+log_entries = []
 
-########################################
+## dictionary for failed logins per ip
+failed_logins_by_ip = {}
 
-        ## REGEX ##
+########### FUNCTIONS ###########
 
+def parse_log_line(line):
 
-        ## IP ADDRESSES ##
-        ## word boundary > 1-3 digits + period > repeat 3 more times > end word boundary
-        ip_match = re.search(r"\b(?:\d{1,3}\.){3}\d{1,3}\b", line) 
+    if line.startswith("#"):
+        return None
 
-        if ip_match:
-            ip_address = ip_match.group()
-            #print(ip_address)
+    # debugging the blank line error when trying to pull parts 
+    if not line.strip():
+        return None
 
-        else:
-            ip_address = "Unknown"
+    ## IP ADDRESSES ##
+    ## word boundary > 1-3 digits + period > repeat 3 more times > end word boundary
+    ip_match = re.search(r"\b(?:\d{1,3}\.){3}\d{1,3}\b", line) 
 
+    if ip_match:
+        ip_address = ip_match.group()
+        #print(ip_address)
 
-
-        ## USERNAMES (finding "user ____" ##
-        ## boundary around 'user' > '/s+' = space, one or more times > '/w+' = word character, one or more (captures the username) ##
-        ## .IGNORECASE debugs an issue in the case where a username might not match due to upper/lowercase characters ##
-        ## group() would give us something like "user zach". With '1' as the argument it returns just the username ##
-        username_match = re.search(r"\buser\s+(\w+)", line, re.IGNORECASE)
-
-        if username_match:
-            username = username_match.group(1)
-            #print(username)
-
-        else:
-            username = "Unknown"
+    else:
+        ip_address = "Unknown"
 
 
 
-        ## EVENT MESSAGES ##
-        parts = line.split()
-        event_message = " ".join(parts[3:])
+    ## USERNAMES (finding "user ____" ##
+    ## boundary around 'user' > '/s+' = space, one or more times > '/w+' = word character, one or more (captures the username) ##
+    ## .IGNORECASE debugs an issue in the case where a username might not match due to upper/lowercase characters ##
+    ## group() would give us something like "user zach". With '1' as the argument it returns just the username ##
+    username_match = re.search(r"\buser\s+(\w+)", line, re.IGNORECASE)
+
+    if username_match:
+        username = username_match.group(1)
+        #print(username)
+
+    else:
+        username = "Unknown"
+
+    ## EVENT MESSAGES ##
+    parts = line.split()
+    event_message = " ".join(parts[3:])
+
+    ## TIMESTAMPS ##
+    timestamp_match = re.search(r"\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}", line)
+
+    if timestamp_match:
+        timestamp_text = timestamp_match.group()
+
+        #### Converting timestamp string into actual date/time ####
+        #### Converting to datetime allows us to use datetime figures with math operators ####
+        timestamp = datetime.strptime(timestamp_text, "%Y-%m-%d %H:%M:%S")
+    else:
+        timestamp = None
 
 
+    ## SEVERITY ##
+    severity_match = re.search(r"\b(INFO|WARNING|ERROR)\b", line)
+    if severity_match:
+        severity = severity_match.group()
+    else:
+        severity = "Unknown"    
 
-        ## TIMESTAMPS ##
-        timestamp_match = re.search(r"\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}", line)
+## STORING LOG ENTRIES ##
+def log_entries():
 
-        if timestamp_match:
-            timestamp_text = timestamp_match.group()
+    log_entries.append({
+        "timestamp": timestamp,
+        "severity": severity,
+        "username": username,
+        "ip_address": ip_address,
+        "event_message": event_message
+    })
 
-            #### Converting timestamp string into actual date/time ####
-            #### Converting to datetime allows us to use datetime figures with math operators ####
-            timestamp = datetime.strptime(timestamp_text, "%Y-%m-%d %H:%M:%S")
-        else:
-            timestamp = None
+    if severity == "INFO":
+        info_count += 1
+    elif severity == "WARNING":
+        warning_count += 1
+    elif severity == "ERROR":
+        error_count += 1
 
-
-        ## SEVERITY ##
-        severity_match = re.search(r"\b(INFO|WARNING|ERROR)\b", line)
-        if severity_match:
-            severity = severity_match.group()
-        else:
-            severity = "Unknown"
-
-#############################################################
-        ### STORING LOG ENTRIES ###
-        log_entries.append({
-            "timestamp": timestamp,
-            "severity": severity,
-            "username": username,
-            "ip_address": ip_address,
-            "event_message": event_message
-        })
-
-        if severity == "INFO":
-            info_count += 1
-        elif severity == "WARNING":
-            warning_count += 1
-        elif severity == "ERROR":
-            error_count += 1
-
-
-    ### FAILED LOGINS ###
+### FAILED LOGINS ###
+def failed_login_data():
 
     for entry in log_entries:
         if "Failed login attempt" in entry["event_message"]:
@@ -137,8 +131,6 @@ with open(LOG_FILE, "r") as file:
                 failed_logins_by_ip[ip]["timestamps"].append(entry["timestamp"])
 
     ## .items() retrieves dictionary items ##
-
-
     for ip, data in failed_logins_by_ip.items():
 
         timestamps = data["timestamps"]
@@ -149,8 +141,24 @@ with open(LOG_FILE, "r") as file:
             if time_diff <= FIVE_MINS:
                 print(
                     f"Potential brute force attempt detected from {ip}: "
-                    f"{data['count']} failed attempts between {timestamps[start]} and {timestamps[start + 4]}"
-                    )
+                    f"{data['count']} failed attempts between {timestamps[start]} and {timestamps[start + 4]}")
+
+#########################
+### MAIN ###
+
+## adding portability for reading the log file ##
+LOG_FILE = Path(__file__).parent / "sample.log"
+
+with open(LOG_FILE, "r") as file:
+    for line in file:
+        entry = parse_log_line(line)
+
+        if entry is None:
+            continue
+
+
+
+#####################################
 
 
 #print(failed_logins_by_ip)

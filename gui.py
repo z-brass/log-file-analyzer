b473cc6a5006e623e698d036e53844add9841553
Zachary Brass <zachary_brass2517@outlook.com>
2026-09-24 17:05:50 -0400

    basic idea setup

--- gui.py
+++ gui.py
@@ -1,1 +1,29 @@
 import tkinter as tk
+from tkinter import filedialog, messagebox
+
+## GUI Goal ##
+'''
+_______________________________________________
+|             Log File Analyzer                |
+|                                              |
+|  Log File:   [no file selected...] [Browse]  |
+|                                              |
+|                   [Analyze]                  |
+|                                              |         
+|  -----------------------------------------   |             
+|                                              |  
+|  Results                                     |   
+|                                              |           
+|  Total Entries: __                           |    
+|  INFO: __                                    |     
+|  WARNING: __                                 |
+|  ERROR: __                                   |
+|                                              |
+|  Security Alerts:                            | 
+|                                              |           
+|______________________________________________|                                                
+'''
+
+## MAIN WINDOW ##
+window = tk.Tk()
+
